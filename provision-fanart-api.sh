e859e2ea8834fa29f1b38ef0e9fdb96ced701e61
KodeStar <kodestar@gmail.com>
2015-09-18 21:04:07 +0100

    checkjing if docker is installed

--- provision-fanart-api.sh
+++ provision-fanart-api.sh
@@ -1,6 +1,5 @@
 #!/bin/bash          
-echo "Provisioning the fanart.tv API"    
-
+MASTER=NO
 for i in "$@"
 do
 case $i in
@@ -19,10 +18,26 @@ case $i in
     echo "  -s | --serverip # Optional / Mandatory if --ip is set, hosts external ip"
     echo "  -g | --gateway # Optional / Mandatory if --ip is set, gateway to use - on ovh this is the main ip with the last octet as 254 on online.net last octet should be 1"
     exit
+    ;;
     *)
             # unknown option
     ;;
 esac
 done
+
+echo "Provisioning the fanart.tv API"    
 echo "APIKEY  = ${APIKEY}"
-echo "MASTER  = ${MASTER}"
+# Check if Docker is installed
+DOCKER=$(docker -v)
+if [ $DOCKER = "bash: docker: command not found" ]; then
+	echo "not installed"
+fi
+
+if [ ${MASTER} = "YES" ]; then
+    echo "Installing Master API"
+    # Do stuff
+else
+     echo "Installing Client API"
+     # Do stuff
+fi
+
